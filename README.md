2a8a18f61346c8d37f3878daad1079aed5973a0e
bitsofinfo <bitsofinfo.g@gmail.com>
2020-07-17 09:16:38 -0400

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # merger.sh
+
+Simple utility for merging branches w/ auto-tagging 
+
+## Usage
+
+```
+merger.sh fromBranchName toBranchName tagName
+```
+
+
+## Example
+
+Clone this repo:
+```
+git clone https://github.com/bitsofinfo/merger.sh
+```
+
+Fork the test repo: https://github.com/bitsofinfo/merge-test-1
+
+Clone your FORK above https://github.com/[your account]/merge-test-1
+```
+git clone https://github.com/[your account]/merge-test-1
+cd merge-test-1
+```
+
+Progress a change through the branches
+```
+git checkout develop
+
+echo "new file" > new-file.txt
+git add new-file.txt
+git commit new-file.txt -m "new-file.txt"
+git push
+```
+
+Lets merge to `qa`, then `master` with auto tagging markers
+```
+merger.sh develop qa qa-NewFileChange
+merger.sh qa master master-NewFileChange
+```
+
+
+
---
+++ merger.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# usage: merger.sh fromBranchName toBranchName tagName
+
+fromBranch=$1
+toBranch=$2
+tagName=$3
+
+# validate
+if [ -z "$fromBranch" ]; then
+    echo "[merger.sh] fromBranch is required: usage: merger.sh fromBranchName toBranchName tagName"
+    exit 1
+fi
+if [ -z "$toBranch" ]; then
+    echo "[merger.sh] toBranch is required: usage: merger.sh fromBranchName toBranchName tagName"
+    exit 1
+fi
+if [ -z "$tagName" ]; then
+    echo "[merger.sh] tagName is required: usage: merger.sh fromBranchName toBranchName tagName"
+    exit 1
+fi
+
+# legit project?
+git status
+exitCode=$?
+if [ "$exitCode" != 0 ]; then
+    echo "[merger.sh] git status returned non-zero exit code: $exitCode. We are not in a git project?"
+    exit 1
+fi
+
+# setup our tag modifier
+tsUtc=$(date -u +%Y%m%d_%H%M%SZ)
+tagName="$tsUtc-$tagName"
+
+# get current branch
+currBranch=$(git branch | grep \* | tr -cd '[:alnum:]')
+echo "[merger.sh] Current branch = $currBranch"
+
+# ensure we are where we need to be (in the target $toBranch)
+if [ "$currBranch" != "$toBranch" ]; then
+    echo "[merger.sh] currBranch[$currBranch] != toBranch[$toBranch], executing 'git checkout $toBranch'...."
+    git checkout $toBranch
+    
+    exitCode = $?
+    if [ "$exitCode" != 0 ]; then
+        echo "[merger.sh] 'git checkout $toBranch' failed with exit-code $exitCode"
+        exit $exitCode
+    fi
+
+    currBranch=$(git branch | grep \* | tr -cd '[:alnum:]')
+    echo "[merger.sh] post-checkout: current branch now = $currBranch"
+fi
+
+# sanity
+if [ "$currBranch" != "$toBranch" ]; then
+    echo "[merger.sh] 'git checkout $toBranch' failed? currBranch[$currBranch] != toBranch[$toBranch]... exiting"
+    exit 1
+fi
+
+# ok lets merge
+git merge $fromBranch
+exitCode=$?
+if [ "$exitCode" != 0 ]; then
+    echo "[merger.sh] 'git merge $fromBranch' from within $currBranch, failed with exit-code $exitCode"
+    exit $exitCode
+fi
+
+echo "[merger.sh] 'git merge $fromBranch' from within $currBranch successful"
+
+
+# ok lets push 
+git push
+exitCode=$?
+if [ "$exitCode" != 0 ]; then
+    echo "[merger.sh] 'git push' from within $currBranch failed with exit-code $exitCode"
+    exit $exitCode
+fi
+
+echo "[merger.sh] 'git push' from within $currBranch successful"
+
+# ok lets tag
+git tag -a $tagName -m "merger.sh $fromBrach -> $toBranch at $tsUtc: tag=$tagName"
+exitCode=$?
+if [ "$exitCode" != 0 ]; then
+    echo "[merger.sh] 'git tag -a $tagName'  from within $currBranch failed with exit-code $exitCode"
+    exit $exitCode
+fi
+
+echo "[merger.sh] 'git tag -a $tagName' from within $currBranch successful"
+
+# and lets push
+git push origin $tagName 
+exitCode=$?
+if [ "$exitCode" != 0 ]; then
+    echo "[merger.sh] 'git push origin $tagName' from within $currBranch failed with exit-code $exitCode"
+    exit $exitCode
+fi
+
+echo "[merger.sh] 'git push origin $tagName' from within $currBranch successful"
+
+exit 0
